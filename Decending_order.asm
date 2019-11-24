c37ca7016778946a31ec02c77712f4d59fa92b0c
Manish Kumar <46210835+gomanish@users.noreply.github.com>
2019-11-24 13:05:41 +0530

    ALP to arrange 10-bytes of data in decending order

---
+++ Decending_order.asm
@@ -0,0 +1,21 @@
+MVI C,10
+MVI B,00
+DCR C
+MVI D,00
+K2: LXI H,100   ;100(Starting address of 10 byte of data)
+MOV B,C
+K1: MOV A,M
+INX H
+CMP M
+JNC L1
+MOV D,A
+MOV A,M
+MOV M,D
+DCX H
+MOV M,A
+INX H
+L1: DCR B
+JNZ K1
+DCR C
+JNZ K2
+HLT
